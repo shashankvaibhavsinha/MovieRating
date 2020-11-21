035cabf4a872f6ee629964df6be47b7cc30ed248
Shashank Vaibhav <shashank.vaibhav@synapse.com>
2020-11-21 16:48:45 +0530

    mdification in services file

--- src/app/movies.ts
+++ src/app/movies.ts
@@ -1,5 +1,11 @@
 export interface Movies {
-    id: number,
+  dates: {
+    maximum: string,
+    minimum: string
+  },
+  page: string,
+  results:{
+    id: string,
     adult: string,
     backdrop_path: string,
     genre_ids: [];
@@ -7,13 +13,15 @@ export interface Movies {
     original_title: string,
     poster_path: string,
     video: boolean,
-    
     title: string,
     overview: string,
     popularity: string,
     release_date: string,
     vote_average: string,
     vote_count: string,
+  }
+  total_pages: string,
+  total_results: string
 }
 
 export interface MoviesData {
--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -3,20 +3,22 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Observable} from 'rxjs';
 import { environment } from './../environments/environment';
+// import { Movies } from 'movies.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
 
-  constructor(private router:Router, private http: HttpClient) { }
+  constructor(private router:Router, 
+              private http: HttpClient) { }
 
   getTopRateMovies(): Observable<any>{
     return this.http.get(environment.topRatedAPI);
   }
 
   getUpcomingMovies(): Observable<any>{
-    return this.http.get(environment.upcomingAPI);
+    return this.http.get<Movies>(environment.upcomingAPI);
   }
 
 }
